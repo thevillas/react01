10b7b438cf0f9d074a2d809ca440cc399ff4ed2b
thevillas <villalbas991@gmail.com>
2023-07-27 18:36:41 -0500

    terminado

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='container'>
+          <div className='row'>
+            <div className='col-nd-6'>
+            <div classname="alert alert-success" role="alert">
+              <h4 classname="alert-heading">Well done!</h4>
+              <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+              <hr/>
+              <p classname="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+              </div>
+            </div>
+            <div className='col-nd-6'>
+            <div classname="alert alert-success" role="alert">
+              <h4 classname="alert-heading">Well done!</h4>
+              <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+              <hr/>
+              <p classname="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
